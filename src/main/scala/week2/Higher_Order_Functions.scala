337b270fabb2c441f3f159a6966116556abb1428
Nick <nikolayvaklinov@yahoo.com>
2020-01-21 15:32:38 +0000

    example for higher order functions(those which consist of other functions)

---
+++ src/main/scala/week2/Higher_Order_Functions.scala
@@ -0,0 +1,11 @@
+package week2
+
+object Higher_Order_Functions extends App {
+  def sum(f:Int => Int, a:Int, b: Int) = {
+    def loop(a:Int, acc:Int):Int =
+      if(a > b) acc
+      else loop(a + 1, f(a) + acc)
+    loop(a,0)
+  }
+
+}
