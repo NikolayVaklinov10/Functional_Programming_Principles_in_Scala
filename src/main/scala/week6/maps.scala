f8d53f09e29bed173d740c50ece81c2afc3e11dd
Nick <nikolayvaklinov@yahoo.com>
2020-01-26 13:51:36 +0000

    maps commit

---
+++ src/main/scala/week6/maps.scala
@@ -0,0 +1,10 @@
+package week6
+
+object maps {
+  val romanNumerals = Map("I" -> 1, "V" -> 5, "X" -> 10)
+
+  val capitalOfCountry = Map("US" -> "Washington", "Switzerland" -> "Bern")
+
+  capitalOfCountry("andorra")
+
+}
