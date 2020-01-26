65b0d71e3ba5335e46d2b3fd53f49e229b26ea33
Nick <nikolayvaklinov@yahoo.com>
2020-01-26 14:20:20 +0000

    adding default value

--- src/main/scala/week6/polynomials.scala
+++ src/main/scala/week6/polynomials.scala
@@ -4,6 +4,7 @@ object polynomials {
 
   class Poly(val terms: Map[Int, Double]){
     def this(bindings: (Int, Double)*) = this(bindings.toMap)
+    val terms = terms0 withDefaultValue 0.0
     def + (other: Poly) = new Poly(terms ++ (other.terms map adjust))
     def adjust(term: (Int, Double)): (Int, Double) = {
       val (exp, coeff) = term
@@ -18,6 +19,7 @@ object polynomials {
   val p1 = new Poly(Map(1 -> 2.0, 3 -> 4.0, 5 -> 6.2))
   val p2 = new Poly(Map(0 -> 3.0, 3 -> 7.0))
   p1 + p2
+  p1.terms(7)
 }
 
 
