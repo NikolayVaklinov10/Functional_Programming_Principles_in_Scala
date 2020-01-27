4ac03c1c1dbd4f0e56c80582d7961b5968dce8d6
Nick <nikolayvaklinov@yahoo.com>
2020-01-27 17:17:10 +0000

    Singleton Object in Scala

---
+++ src/main/scala/week6/Logger.scala
@@ -0,0 +1,6 @@
+package week6
+
+object Logger {
+  def info(message:String):Unit = println("Info:"+message)
+
+}
---
+++ src/main/scala/week6/Project.scala
@@ -0,0 +1,12 @@
+package week6
+import week6.Logger.info
+
+class Project(name:String, days:Int)
+
+class Test{
+
+  val project1 = new Project("Book writing", 365)
+  val project2 = new Project("Revision", 30)
+  info("Planned")
+
+}
